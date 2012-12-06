a7a594bb938bce4cb838f9bb1e89cdc824b555fd
Korneliusz Caputa <helluinster@gmail.com>
2012-12-06 09:44:27 +0100

    Adding the mouse handler.

---
+++ Tanks3DFPP/Tanks3DFPP/Utilities/MouseHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tanks3DFPP.Utilities
+{
+    public static class MouseHandler
+    {
+        public static Vector2 GetPositionDifference(MouseState sourceState)
+        {
+            MouseState currentState = Mouse.GetState();
+            return new Vector2(currentState.X - sourceState.X, currentState.Y - sourceState.Y);
+        }
+    }
+}
