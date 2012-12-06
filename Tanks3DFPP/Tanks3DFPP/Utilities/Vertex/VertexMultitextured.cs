b9dc072ab82b5332a5d8b9b49bf60fbbdef1d87d
Korneliusz Caputa <helluinster@gmail.com>
2012-12-06 09:37:28 +0100

    Adding vertexmultitextured

---
+++ Tanks3DFPP/Tanks3DFPP/Utilities/Vertex/VertexMultitextured.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tanks3DFPP.Utilities
+{
+    public struct VertexMultitextured
+    {
+        public Vector3 Position;
+        public Vector3 Normal;
+        public Vector4 TextureCoordinate;
+        // Vector4 is kinda hacky in this context as we might want to use more levels in the future.
+        // However, it's compile-time ready, so there is less work with the VertexElements decalaration.
+        public Vector4 TextureWeights;
+
+        public static int SizeInBytes = (3 + 3 + 4 + 4) * sizeof(float);
+        public static VertexElement[] VertexElements = new VertexElement[]
+        {
+            new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+            new VertexElement(sizeof(float) * 3, VertexElementFormat.Vector3, VertexElementUsage.Normal, 0),
+            new VertexElement(sizeof(float) * 6, VertexElementFormat.Vector4, VertexElementUsage.TextureCoordinate, 0),
+            new VertexElement(sizeof(float) * 10, VertexElementFormat.Vector4, VertexElementUsage.TextureCoordinate, 1)
+        };
+
+        public static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration(VertexElements);
+    }
+}
