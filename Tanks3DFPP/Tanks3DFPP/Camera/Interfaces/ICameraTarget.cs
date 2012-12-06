ebae129c017854b97977c39ee62330239c25b081
Korneliusz Caputa <helluinster@gmail.com>
2012-12-06 09:45:05 +0100

    ICameraTarget interface.

---
+++ Tanks3DFPP/Tanks3DFPP/Camera/Interfaces/ICameraTarget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Tanks3DFPP.Camera.Interfaces
+{
+    /// <summary>
+    ///     This may disappear in some time. :P
+    /// </summary>
+    public interface ICameraTarget
+    {
+        float FacingDirection { get; }
+
+        Vector3 Position { get; }
+    }
+}
